305aae73ccdd42a42aac82a58284efc6b97803cd
kemsmahen <kamalnurmahendra1@gmail.com>
2025-02-27 15:30:42 +0700

    Perubahan Terbaru 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Kam + Nis</h1>
+      <h1>Kamal + Nisrina </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
